281d8fd9962b2e0ef02b6bb16ef2ca49d9905be8
sageyu34 <sageyuria@gmail.com>
2026-06-11 06:26:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,28 +2,50 @@ const SUPABASE_URL = "https://crsrillwsyemmqlodptp.supabase.co";
 const SUPABASE_KEY = "sb_publishable_eExNt51cdrssnoo6XVgQ4A_tfmlWPnk";
 
 const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
+  SUPABASE_URL,
+  SUPABASE_KEY
 );
 
-async function testConnection() {
+const loginForm =
+  document.getElementById("loginForm");
 
-    const { data, error } = await supabaseClient
+loginForm.addEventListener(
+  "submit",
+  async (event) => {
+
+    event.preventDefault();
+
+    const memberId =
+      document.getElementById("memberId")
+      .value
+      .trim()
+      .toUpperCase();
+
+    const pin =
+      document.getElementById("pin")
+      .value
+      .trim();
+
+    const { data, error } =
+      await supabaseClient
         .from("members")
-        .select("*");
+        .select("*")
+        .eq("member_id", memberId)
+        .eq("pin", pin)
+        .single();
 
     const message =
-        document.getElementById("message");
+      document.getElementById("message");
 
-    if (error) {
-        message.textContent =
-            "Error: " + error.message;
+    if (error || !data) {
 
-        return;
+      message.textContent =
+        "Invalid Member ID or PIN";
+
+      return;
     }
 
     message.textContent =
-        JSON.stringify(data, null, 2);
-}
-
-testConnection();
+      `Welcome, ${data.full_name}`;
+  }
+);
